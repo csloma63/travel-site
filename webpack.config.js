6548cbd32eac577e76fdd07f5a0b22c8e5c04c3c
Chris <csloma42@gmail.com>
2018-02-21 12:18:46 -0500

    completed mobile header

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
       loaders: [
         {
-          loader: 'babel',
+          loader: 'babel-loader',
           query: {
             presets: ['es2015']
           },
